16ff7432ec3500e1a640cb15794e2b9c64763fd3
Daniil Popov <popovdanin@gmail.com>
2025-03-02 23:44:43 +0300

    Initial commit

---
+++ src/main/java/org/skyhigh/notessearchservice/NotesSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package org.skyhigh.notessearchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesSearchServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotesSearchServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/skyhigh/notessearchservice/NotesSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skyhigh.notessearchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesSearchServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
